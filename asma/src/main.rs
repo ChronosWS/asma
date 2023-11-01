6257a313ee1cdf0d1af9f9296b9149b77d85e48c
Cliff Hudson <cliff.s.hudson@gmail.com>
2023-10-31 21:52:17 -0700

    Allow installs to selected directory

--- asma/src/main.rs
+++ asma/src/main.rs
@@ -87,21 +87,74 @@ fn main() -> Result<(), slint::PlatformError> {
     // TODO: Error handling - we are currently just printing errors to the console, we should gather those up from
     // our async calls and log them somewhere, and possibly raise a dialog box for those operations which require it
     // TODO: Boilerplate - most of this call is boilerplate. Possibly a good candidate for a macro.
+    let ui_handle2 = ui_handle.clone();
     ui.on_refresh_ip(move || {
         println!("refresh_ip pressed!");
-        let ui_handle = ui_handle.clone();
+        let ui_handle = ui_handle2.clone();
         ASYNC_RUNTIME.spawn(async move {
             let ip = refresh_ip().await;
             if let Ok(ip) = ip {
                 ui_handle
-                .upgrade_in_event_loop(move |handle| handle.set_ip(ip.to_string().into()))
-                .map_err(|e| eprintln!("Failed to execute callback in event loop: {}", e.to_string()))
+                    .upgrade_in_event_loop(move |handle| handle.set_ip(ip.to_string().into()))
+                    .map_err(|e| {
+                        eprintln!(
+                            "Failed to execute callback in event loop: {}",
+                            e.to_string()
+                        )
+                    })
             } else {
                 Err(())
             }
         });
     });
-    
+
+    let ui_handle2 = ui_handle.clone();
+    ui.on_profile_set_location(move || {
+        println!("profile_set_location pressed!");
+        let folder = rfd::FileDialog::new()
+            .set_title("Select installation directory")
+            .pick_folder();
+        if let Some(folder) = folder {
+            if let Some(folder) = folder.to_str() {
+                let ui = ui_handle2.unwrap();
+                let mut profile = ui.get_profile();
+                profile.installation_location = folder.into();
+                ui.set_profile(profile);
+            }
+        }
+    });
+
+    let ui_handle2 = ui_handle.clone();
+    ui.on_profile_install(move || {
+        println!("profile_install pressed!");
+        let ui = ui_handle2.unwrap();
+        let profile = ui.get_profile();
+        if std::fs::create_dir_all(profile.installation_location.as_str()).is_ok() {
+            let steamcmd = std::process::Command::new("E:\\Games\\SteamCMD\\steamcmd.exe")
+                .args([
+                    "+force_install_dir",
+                    profile.installation_location.as_str(),
+                    "+login",
+                    "anonymous",
+                    "+app_update",
+                    "2430930",
+                    "validate",
+                    "+quit",
+                ])
+                .stdout(std::process::Stdio::inherit())
+                .spawn()
+                .map_err(|e| eprintln!("Failed to start steamcmd: {}", e.to_string()));
+            if let Ok(mut steamcmd) = steamcmd {
+                let result = steamcmd.wait().map_err(|e| eprintln!("steamcmd errored: {}", e.to_string()));
+                if let Ok(exit_status) = result {
+                    println!("steamcmd exited with code {}", exit_status.to_string());
+                }
+            }
+        } else {
+            eprintln!("Failed to create directory: {}", profile.installation_location);
+        }
+    });
+
     // This starts the event loop and doesn't return until the application window is closed.
     ui.run()
 }
